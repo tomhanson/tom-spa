9f0fff0d644ec5413016939b7bdd27805d8d6f67
tomhanson <tomhanson1985@gmail.com>
2017-06-21 09:16:26 +0100

    more testing service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,24 +11,24 @@ if (navigator.serviceWorker) {
 }
 
 
-
-const CACHE_VERSION = 'app-v1';
-const CACHE_FILES = [
-    '/index.html',
-    '/grid-polyfill.js',
-    'favicon.ico',
-    'static/css/*',
-    'static/js/*',
-    'https://fonts.googleapis.com/css?family=Lato:100|Roboto+Condensed:300,400,700',
-    // 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo'
-];
-
-self.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open(CACHE_VERSION)
-            .then(function (cache) {
-                console.log('Opened cache', CACHE_FILES);
-                return cache.addAll(CACHE_FILES);
-            })
-    );
-});
+//
+// const CACHE_VERSION = 'app-v1';
+// const CACHE_FILES = [
+//     '/index.html',
+//     '/grid-polyfill.js',
+//     'favicon.ico',
+//     'static/css/*',
+//     'static/js/*',
+//     'https://fonts.googleapis.com/css?family=Lato:100|Roboto+Condensed:300,400,700',
+//     // 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo'
+// ];
+//
+// self.addEventListener('install', function (event) {
+//     event.waitUntil(
+//         caches.open(CACHE_VERSION)
+//             .then(function (cache) {
+//                 console.log('Opened cache', CACHE_FILES);
+//                 return cache.addAll(CACHE_FILES);
+//             })
+//     );
+// });
